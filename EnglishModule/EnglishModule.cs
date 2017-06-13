a43eb6cc3e04a6c7389c356b69ce36b85a82be97
SubasishMohapatra <subasish.mohapatra@hotmail.com>
2017-06-13 16:12:15 +0530

    Added another module

---
+++ EnglishModule/EnglishModule.cs
@@ -0,0 +1,28 @@
+﻿using EnglishModule.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishModule
+{
+    public class EnglishModule:IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public EnglishModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            //Register all views to be displayed
+            this.regionManager.RegisterViewWithRegion("NavigationRegion", typeof(EnglishMenuItemView));
+            this.regionManager.RegisterViewWithRegion("ContentRegion", typeof(EnglishContentView));
+        }
+    }
+}
